00233561c318ee4ec3b777464afefd06390ff705
arihant.jain <arihantjain266@gmail.com>
2023-04-08 00:38:15 +0530

    Arihant| updating two sum problem

--- src/main/java/org/arihant/twoSum.java
+++ src/main/java/org/arihant/twoSum.java
@@ -5,34 +5,27 @@ import java.util.*;
 public class twoSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        ArrayList<Integer> list = new ArrayList<>();
-//        System.out.println("Print the size of the array : ");
-//        int size = sc.nextInt();
-//        System.out.println("Enter the numbers of the array : ");
-//        for (int i=0;i<size;i++){
-//            list.add(sc.nextInt());
-//        }
-        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 20};
         System.out.println("Enter the number whose sum needs to be searched for: ");
         int target = sc.nextInt();
         twoSumCheck(input, target);
     }
-    private static int[] twoSumCheck(int[] list, int sum){
+    private static int[] twoSumCheck(int[] num, int sum){
         Map<Integer,Integer> map = new HashMap<>();
-        if(list.length!=0){
-            for(int i=0; i<list.length;i++){
-            int complement = sum - list[i];
-            if (map.containsKey(complement)){
-                return new int[] {map.get(complement),i};
+        if(num.length!=0){
+            for(int i=0; i<num.length;i++){
+            int complement = sum - num[i];
+            if (map.containsKey(complement)) {
+                int index1 = map.get(complement)+1;
+                int index2 = i+1;
+                System.out.println("values: "+index1+","+ index2 );
             }
-            else {
-                map.put(list[i],i);
-            }
+                map.put(num[i],i);
+
         }}
         else {
             System.out.println("The list is empty!!");
         }
-        System.out.println(map);
         return null;
     }
 
