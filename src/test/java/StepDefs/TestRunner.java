1fff5f22cd315716f9dd951d674fd0ddabb0bb88
arihant.jain <arihantjain266@gmail.com>
2023-04-18 16:07:38 +0530

    Updating the test runner file

--- src/test/java/StepDefs/TestRunner.java
+++ src/test/java/StepDefs/TestRunner.java
@@ -7,10 +7,11 @@ import org.junit.runner.RunWith;
 //"src/test/java/StepDefs"
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="classpath:Features",
-        glue={"classpath:StepDefs"},
-        plugin = {"pretty", "html:target/cucumber-html-report.html"},
-        monochrome = true)
+@CucumberOptions(features="classpath:Features", //path of the feature files
+        glue={"classpath:StepDefs"}, //path of the step definitions
+        plugin = {"pretty", "html:target/cucumber-html-report.html", "json:target/json-output.json","junit:reports/junit-report.xml"}, //to generate reporting format
+        dryRun = false, //When it is true it would check if any of the step definitions are missing
+        monochrome = true) //displaying the output in the readable format
 public class TestRunner {
 
 }
